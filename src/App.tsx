a228aa15110efe601a3df2b0503b42e6de2383df
Dieferson <dieferson@icaro.local>
2021-10-07 17:30:42 -0300

    atualizado 07/10/2021 1730

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,38 @@ import { items } from './data/items'
 import { categories } from './data/categories'
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
 import { TableArea } from './components/tableArea'
+import { InfoArea } from './components/InfoArea'
 
 const App = ()=> {
   const [list, setList] = useState(items)
   const [filterList, setFilterList] = useState<Item[]>([])
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
+  const [income, setIncome] = useState(0)
+  const [expense, setExpense] = useState(0)
   
   useEffect(()=>{
     setFilterList( filterListByMonth(list, currentMonth))
   },[list, currentMonth])
 
+  useEffect(()=>{
+    let IncomeCount = 0
+    let expenseCount = 0
+    for(let i in filterList){
+      if(categories[filterList[i].category].expense){
+        expenseCount += filterList[i].value
+      }else{
+        IncomeCount += filterList[i].value
+      }
+    }
+
+    setIncome(IncomeCount)
+    setExpense(expense)
+
+  },[filterList])
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth)
+  }
   return(
     <C.Container>
       <C.Header>
@@ -25,8 +47,14 @@ const App = ()=> {
       </C.Header>
       <C.Body>
 
-        {/* ÁREA DE INFORMAÇÕES */}
-      
+        <InfoArea 
+          currentMonth={currentMonth} 
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+          />
+
+
         {/* ÁREA DE INSERÇÃO */}
 
         <TableArea list={filterList} ></TableArea>
