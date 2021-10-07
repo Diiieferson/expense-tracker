23d584d5557e3fe5e2bb216c67adb429f23b6ced
Dieferson de Oliveira <diiieferson@gmail.com>
2021-10-07 02:05:08 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import * as C from './App.styles'
+import { Item } from './types/Item'
+import { Category } from './types/category'
+import { items } from './data/items'
+import { categories } from './data/categories'
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
+import { TableArea } from './components/tableArea'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=> {
+  const [list, setList] = useState(items)
+  const [filterList, setFilterList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
+  
+  useEffect(()=>{
+    setFilterList( filterListByMonth(list, currentMonth))
+  },[list, currentMonth])
+
+  return(
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>
+          Sistema Financeiro 
+        </C.HeaderText>
+      </C.Header>
+      <C.Body>
+
+        {/* ÁREA DE INFORMAÇÕES */}
+      
+        {/* ÁREA DE INSERÇÃO */}
+
+        <TableArea list={filterList} ></TableArea>
+      
+      
+      </C.Body>
+    </C.Container>
+  )
 }
 
-export default App;
+export default App
